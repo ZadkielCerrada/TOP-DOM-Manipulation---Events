360a78180afd6328d123d606ae276f9c9b677838
Zadkiel Cerrada <mr.cerrada.zadkiel@gmail.com>
2022-08-02 23:13:36 -0700

    createElement ans

--- querySelectors/app.js
+++ querySelectors/app.js
@@ -1,13 +1,9 @@
-// query selectors
+const container = document.querySelector("#container");
+const content = document.querySelector(".content");
+const pRed = document.querySelector("#p-red");
+const h3Blue = document.querySelector("#h3-blue");
+const divPink = document.querySelector("#div-pink");
 
-// const container = document.querySelector("#container");
-// const content = document.querySelector(".content");
-// const pRed = document.querySelector("#p-red");
-// const h3Blue = document.querySelector("#h3-blue");
-// const divPink = document.querySelector("#div-pink");
-
-// document.getElementById("p-red").style.color = "red";
-// document.getElementById("h3-blue").style.color = "blue";
-// document.getElementById("div-pink").style.backgroundColor = "pink";
-
-// --------------------------------------------------------
+document.getElementById("p-red").style.color = "red";
+document.getElementById("h3-blue").style.color = "blue";
+document.getElementById("div-pink").style.backgroundColor = "pink";

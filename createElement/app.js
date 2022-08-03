6421dc43ce624947ba51c38bf9174158f0afc2e2
Zadkiel Cerrada <mr.cerrada.zadkiel@gmail.com>
2022-08-02 23:16:55 -0700

    Delete useless code

--- createElement/app.js
+++ createElement/app.js
@@ -3,26 +3,20 @@ const container = document.querySelector("#container");
 // Elements
 
 const content = document.createElement("div");
-content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 const pRed = document.createElement("p");
-pRed.classList.add("p-red");
 pRed.textContent = "Hey I'm red!";
 
 const h3Blue = document.createElement("h3");
-h3Blue.classList.add("h3-blue");
 h3Blue.textContent = "I'm a blue h3";
 
 const divPink = document.createElement("div");
-divPink.classList.add("div-pink");
 
 const divPinkH1 = document.createElement("h1");
-divPinkH1.classList.add("div-pink-h1");
 divPinkH1.textContent = "I'm in a div";
 
 const divPinkP = document.createElement("p");
-divPinkP.classList.add("div-pink-p");
 divPinkP.textContent = "ME TOO!";
 
 // Append
@@ -31,6 +25,7 @@ container.appendChild(content);
 container.appendChild(pRed);
 container.appendChild(h3Blue);
 container.appendChild(divPink);
+
 divPink.appendChild(divPinkH1);
 divPink.appendChild(divPinkP);
 

fe87a484c8792f45f148bbb615013d82a93f500e
Zadkiel Cerrada <mr.cerrada.zadkiel@gmail.com>
2022-08-02 22:56:06 -0700

    Add query selectors ans

---
+++ querySelectors/app.js
@@ -0,0 +1,13 @@
+// query selectors
+
+// const container = document.querySelector("#container");
+// const content = document.querySelector(".content");
+// const pRed = document.querySelector("#p-red");
+// const h3Blue = document.querySelector("#h3-blue");
+// const divPink = document.querySelector("#div-pink");
+
+// document.getElementById("p-red").style.color = "red";
+// document.getElementById("h3-blue").style.color = "blue";
+// document.getElementById("div-pink").style.backgroundColor = "pink";
+
+// --------------------------------------------------------
